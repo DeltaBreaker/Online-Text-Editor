1aa8aec4e1f5a272ebec535c038300439ab877a8
willc <willcrain1@gmail.com>
2022-12-01 23:25:26 -0500

    done

---
+++ client/src/js/database.js
@@ -0,0 +1,46 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+export const putDb = async (content) => {
+  console.log("Post to the database");
+
+  const jateDb = await openDB("jate", 1);
+
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.add({
+    content,
+  });
+
+  const result = await request;
+};
+
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  const jateDb = await openDB("jate", 1);
+
+  const tx = jateDb.transaction("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+  return result[result.length - 1].content;
+};
+
+initdb();
